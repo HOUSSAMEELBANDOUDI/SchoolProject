3dae9d2a3b261bfacdc2fee43db887b0759cb93e
Houssame El Bandoudi <houssameelbandoudi@gmail.com>
2026-03-31 19:55:36 +0100

    Add Student repository, migrations, and controller
    - Add IStudentRepository interface and StudentRepository implementation
    - Implement CRUD operations (GetAll, Create, Delete, Register)
    - Add InitialCreate migration for database schema
    - Add StudentController

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Repositry/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolProject.Models;
+
+namespace SchoolProject.Repositry
+{
+    public interface IStudentRepository
+    {
+        public List<Student> GetAllStudents();
+        public void Creat(Student student);
+        public void Delete(int id);
+        public void register(int studentId, int courseIs);
+    }
+}
